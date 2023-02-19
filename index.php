769b9191f3c1b1434cb5c265d18e5da12d2c5e09
BimaaTio <bimaatio10@gmail.com>
2023-02-19 15:31:42 +0700

    ok

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require 'config/functions.php';
-$w = query("SELECT * FROM tb_setting")[0];
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,7 +8,7 @@ $w = query("SELECT * FROM tb_setting")[0];
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?= $w['singkatan'] ?> &mdash; Landing</title>
+  <title>APPM &mdash; Landing</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,7 +16,7 @@ $w = query("SELECT * FROM tb_setting")[0];
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="shortcut icon" href="assets/img/foto/<?= $w['logo'] ?>" alt="logo" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
   <style>
     * {
       font-family: 'Poppins', sans-serif;
@@ -37,13 +36,8 @@ $w = query("SELECT * FROM tb_setting")[0];
         <span class="w-100 d-lg-none d-block">
           <!-- hidden spacer to center brand on mobile --></span>
         <a class="navbar-brand" href="#">
-<<<<<<< HEAD
           <img src="assets/img/logo.png" alt="Bootstrap" width="58" height="54" class="me-2">
           Aplikasi Pengaduan Pelaporan Masyarakat </a>
-=======
-          <img src="assets/img/foto/<?= $w['logo'] ?>" alt="Bootstrap" width="58" height="54" class="me-2">
-          <?= $w['nama_web'] ?> </a>
->>>>>>> d9bda3e3ed7f627541252e8c0011462dcd48582a
         <div class="w-100 text-right">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar7">
             <span class="navbar-toggler-icon"></span>
@@ -58,6 +52,9 @@ $w = query("SELECT * FROM tb_setting")[0];
             </li>
           <?php endif; ?>
           <li class="nav-item">
+            <a href="register.php" class="nav-link">Register</a>
+          </li>
+          <li class="nav-item">
             <a href="login.php" class="nav-link">Login</a>
           </li>
         </ul>
@@ -69,7 +66,7 @@ $w = query("SELECT * FROM tb_setting")[0];
   <div class="container col-xxl-8 ">
     <div class="row align-items-center g-5 py-5">
       <div class="col-lg-6" data-aos="fade-right">
-        <h1 class="display-5 fw-bold lh-1 mb-3 "><?= $w['nama_web'] ?></h1>
+        <h1 class="display-5 fw-bold lh-1 mb-3 ">Aplikasi Pelaporan Pengaduan</h1>
         <p class="lead  my-4">
           Suara Anda adalah Perubahan!
         </p>
@@ -95,7 +92,7 @@ $w = query("SELECT * FROM tb_setting")[0];
     <div class="row" data-aos="fade-right">
       <div class="col-md-7">
         <p>
-          <?= $w['deskripsi'] ?>
+          APPM Adalah Aplikasi Pelaporan Pengaduan Masyarakat Berbasis Website,jadi jika disekitar anda ada masalah yang bersangkutan dengan Desa anda dapat melaporkan masalah tersebut disini!
         </p>
       </div>
     </div>
@@ -107,10 +104,10 @@ $w = query("SELECT * FROM tb_setting")[0];
         <h2 class="fw-bold mb-4" data-aos="fade-left">Bagaimana Cara Lapor ?</h2>
         <h6 data-aos="fade-left" class="mb-2">Cara Melaporkan Masalah sebagai Berikut :</h6>
         <ul class="list-unstyled">
-          <li>1. Silahkan Buat Akun Terlebih dahulu</li>
-          <li>2. Setelah Buat Akun,silakan login</li>
-          <li>3. Setelah Login anda akan masuk ke halaman dashboard</li>
-          <li>4. Dan silakan anda mengisi form yang tersedia</li>
+          <li data-aos="fade-left">1. Silahkan Buat Akun Terlebih dahulu</li>
+          <li data-aos="fade-left">2. Setelah Buat Akun,silakan login</li>
+          <li data-aos="fade-left">3. Setelah Login anda akan masuk ke halaman dashboard</li>
+          <li data-aos="fade-left">4. Dan silakan anda mengisi form yang tersedia</li>
         </ul>
       </div>
     </div>
@@ -127,7 +124,7 @@ $w = query("SELECT * FROM tb_setting")[0];
       <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
         <li class="ms-3"><a class="text-muted" href="https://instagram.com/bimatio_"><i class="bi bi-instagram" width="24" height="24"></i></a></li>
         <li class="ms-3"><a class="text-muted" href="https://wa.me/6288802791267"><i class="bi bi-whatsapp" width="24" height="24"></i></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-linkedin" width="24" height="24"></i></a></li>
+        <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-facebook" width="24" height="24"></i></a></li>
       </ul>
     </footer>
   </div>
